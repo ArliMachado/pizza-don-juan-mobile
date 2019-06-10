9b84f2831854bfe1112260dc896b55fe88f23bef
Arli Machado <arli.machado@b2wdigital.com>
2019-06-10 15:43:30 -0300

    Definindo tela de Cadastro de usuario

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,12 @@
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 
 import Login from './pages/Login';
+import CadastroUsuario from './pages/CadastroUsuario';
 
 const Routes = createAppContainer(
   createSwitchNavigator({
     Login,
+    CadastroUsuario,
   }),
 );
 export default Routes;
