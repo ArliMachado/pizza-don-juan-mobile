6873c44ba2ddfab056112d4962672cf65b8f8804
Arli Machado <arli.machado@gmail.com>
2019-06-16 13:50:32 -0300

    Incluindo interceptor para adicionar o token do header das requisições

--- src/services/api.js
+++ src/services/api.js
@@ -1,10 +1,27 @@
 import axios from 'axios';
+import { AsyncStorage } from 'react-native';
+import { TOKEN_KEY, onSignOut } from './auth';
 
 const baseUrlHome = 'http://192.168.1.107:3333';
 const baseUrlJob = 'http://10.168.69.163:3333';
 
 const api = axios.create({
   baseURL: baseUrlHome,
+});
+
+api.interceptors.request.use(async (config) => {
+  try {
+    const token = await AsyncStorage.getItem(TOKEN_KEY);
+
+    if (!token) {
+      onSignOut();
+    } else {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+  } catch (err) {
+    console.tron.log(err);
+  }
+  return config;
 });
 
 export default api;
