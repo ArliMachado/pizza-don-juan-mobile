e2cf759419b92b9d2a444faba05d183ff8c6a5a6
Arli Machado <arli.machado@b2wdigital.com>
2019-06-17 13:50:02 -0300

    Aplicando header em productTypes

--- src/pages/ProductTypes/index.js
+++ src/pages/ProductTypes/index.js
@@ -2,16 +2,20 @@ import React, { Component } from 'react';
 
 import { View, Text } from 'react-native';
 import Container from '~/components/Container';
+import Header from '~/components/Header';
 
 // import { Container } from './styles';
 
 export default class ProductTypes extends Component {
+  backToProducts() {
+    const { navigation } = this.props;
+    navigation.navigate('Menu');
+  }
+
   render() {
     return (
       <Container>
-        <View>
-          <Text>Teste</Text>
-        </View>
+        <Header title="Selecione um tipo" navigateTo={this.backToProducts} />
       </Container>
     );
   }
