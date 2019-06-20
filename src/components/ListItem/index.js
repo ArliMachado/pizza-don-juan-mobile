da9d2442f29da3e825df31793bc69785346bb670
Arli Machado <arli.machado@gmail.com>
2019-06-20 01:14:32 -0300

    Refatorando ListItem

--- src/components/ListItem/index.js
+++ src/components/ListItem/index.js
@@ -1,18 +1,33 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import { Container, Title, Image } from './styles';
+import {
+  Container, Image, InfoContent, Title, Price,
+} from './styles';
 
-const ListItem = ({ title, uri }) => (
+const ListItem = ({ title, uri, price }) => (
   <Container>
     <Image source={{ uri }} />
-    <Title>{title}</Title>
+    <InfoContent>
+      <Title>{title}</Title>
+      {price ? (
+        <Price>
+          R$
+          {price}
+        </Price>
+      ) : null}
+    </InfoContent>
   </Container>
 );
+
+ListItem.defaultProps = {
+  price: null,
+};
 
 ListItem.propTypes = {
   title: PropTypes.string.isRequired,
   uri: PropTypes.string.isRequired,
+  price: PropTypes.string,
 };
 
 export default ListItem;
--- src/components/ListItem/styles.js
+++ src/components/ListItem/styles.js
@@ -9,17 +9,27 @@ export const Container = styled.View`
   margin-bottom: ${metrics.baseMargin / 2};
   align-items: center;
   width: ${(metrics.screenWidth - 45) / 2};
+  height: 200;
   padding: ${metrics.basePadding}px;
-`;
-
-export const Title = styled.Text`
-  text-align: center;
-  font-size: 16;
-  font-weight: bold;
-  color: ${colors.darker};
 `;
 
 export const Image = styled.Image`
   width: 100;
   height: 100;
+`;
+
+export const InfoContent = styled.View`
+  flex: 1;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+export const Title = styled.Text`
+  font-size: 16;
+  font-weight: bold;
+  color: ${colors.darker};
+`;
+export const Price = styled.Text`
+  font-size: 14;
+  color: ${colors.regular};
 `;
